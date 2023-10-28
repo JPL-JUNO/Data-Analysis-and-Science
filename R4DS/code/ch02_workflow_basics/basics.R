b1792fa79b2d5e89237604f9022e7817116beea3
Stephen CUI <junocuixuan@163.com>
2023-10-28 15:37:47 +0800

    工作流基础

---
+++ R4DS/code/ch02_workflow_basics/basics.R
@@ -0,0 +1,19 @@
+1 / 200 * 30
+(59 + 73 + 2) / 3
+sin(pi / 2)
+
+x <- 3 * 4
+
+# create vector of primes
+primes <- c(2, 3, 5, 7, 11, 13)
+
+# multiply primes by 2
+primes * 2
+primes - 1
+
+
+this_is_a_really_long_name <- 2.5
+
+seq(from = 1, to = 10)
+
+seq(1, 10)
