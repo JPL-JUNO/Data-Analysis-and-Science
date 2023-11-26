dce5c2e1b6a9caffde5a56b76c32f508dd5ab1cc
Stephen CUI <junocuixuan@163.com>
2023-11-26 22:49:05 +0800

    提供一些计算函数

--- TSFP/code/utils/funcs.py
+++ TSFP/code/utils/funcs.py
@@ -7,7 +7,30 @@
 """
 
 import numpy as np
+from numpy import ndarray
 
 
 def mape(y_true, y_pred) -> float:
     return 100 * np.mean(np.abs((y_true - y_pred) / y_true))
+
+
+def simulate_process(is_stationary: bool) -> ndarray:
+    np.random.seed(42)
+
+    process = np.zeros(400)
+    if is_stationary:
+        alpha = .5
+    else:
+        alpha = 1
+    for i in range(399):
+        process[i+1] = alpha * process[i] + np.random.standard_normal()
+
+    return process
+
+
+def mean_over_time(ts: ndarray) -> list:
+    return [np.mean(ts[:i]) for i in range(1, len(ts))]
+
+
+def var_over_time(ts: ndarray) -> list:
+    return [np.var(ts[:i]) for i in range(1, len(ts))]
