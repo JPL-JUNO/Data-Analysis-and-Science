ff26b2e584071ceafc155c9055f6b66ca0851656
Stephen CUI <junocuixuan@163.com>
2024-03-11 21:11:21 +0800

    组合 pandas 对象 concat

--- P1XC2E/code/combining_pandas_objects.py
+++ P1XC2E/code/combining_pandas_objects.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 
 bball_16 = pd.read_csv("../data/baseball16.csv")
 assert bball_16.shape == (16, 22)
 
 data_dict = bball_16.iloc[0].to_dict()
 data_dict
+
+new_data_dict = {k: "" if isinstance(v, str) else np.nan for k, v in data_dict.items()}
+new_data_dict
