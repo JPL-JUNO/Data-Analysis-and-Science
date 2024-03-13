8bc6a0653fa21306bf6af6eaec2b6fa343a5f48d
Stephen CUI <junocuixuan@163.com>
2024-03-14 00:03:38 +0800

    groupby 分组

--- PIA/code/ch09.py
+++ PIA/code/ch09.py
@@ -16,3 +16,106 @@ food_data = {
 }
 supermarket = pd.DataFrame(data=food_data)
 supermarket
+
+groups = supermarket.groupby("Type")
+groups
+
+groups.get_group("Fruit")
+groups.get_group("Vegetable")
+
+groups.mean(numeric_only=True)
+
+fortune = pd.read_csv("../data/fortune1000.csv")
+fortune
+
+retailing_mask = fortune["Sector"] == "Retailing"
+retail_companies = fortune[retailing_mask]
+retailing_mask.head()
+retail_companies["Revenues"].head()
+retail_companies["Revenues"].mean()
+
+sectors = fortune.groupby("Sector")
+
+len(sectors)
+
+fortune["Sector"].nunique()
+
+sectors.size()
+
+sectors.groups
+
+sectors.first()
+
+sectors.last()
+
+sectors.nth(0)
+
+sectors.head()
+
+sectors.tail()
+
+sectors.get_group("Apparel")
+
+sectors.sum(numeric_only=True).head(3)
+
+sectors.mean(numeric_only=True).head()
+
+sectors["Revenues"]
+
+sectors["Revenues"].max().head(1)
+sectors["Revenues"].min().head(1)
+
+aggregations = {"Revenues": "min", "Profits": "max", "Employees": "mean"}
+sectors.agg(aggregations).head()
+
+fortune.nlargest(5, columns="Profits")
+
+
+def get_largest_row(df):
+    return df.nlargest(1, "Revenues")
+
+
+sectors.apply(get_largest_row).head()
+
+
+def get_largest_row(df, columns="Revenues"):
+    return df.nlargest(1, columns=columns)
+
+
+sectors.apply(get_largest_row, columns="Profits")
+
+
+def get_smallest_row(df, columns="Profits"):
+    return df.nsmallest(1, columns=columns)
+
+
+def get_max(df, columns):
+    return df[columns].max()
+
+
+def get_min(df, columns):
+    return df[columns].min()
+
+
+def get_value(df, columns):
+    return (get_min(df, columns), get_max(df, columns))
+
+
+def get_max(df, columns):
+    return df[columns].max()
+
+
+def get_min(df, columns):
+    return df[columns].min()
+
+
+sectors.apply(
+    lambda x: pd.Series(
+        {
+            "Profits": get_max(x, columns="Profits"),
+            "Revenues": get_min(x, columns="Revenues"),
+        }
+    )
+)
+
+sectors.apply(get_value, columns="Profits").head()
