bb3c92eb87ab7190e5e63d800b463197de536477
Stephen CUI <junocuixuan@163.com>
2024-08-04 23:57:02 +0800

    选择数据子集

---
+++ P1XC2E/sources/ch06_selecting_subsets_of_data.py
@@ -0,0 +1,17 @@
+"""
+@File         : ch06_selecting_subsets_of_data.py
+@Author(s)    : Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime  : 2024-08-01 16:42:20
+@Email        : cuixuanstephen@gmail.com
+@Description  : 选择数据子集
+"""
+
+import pandas as pd
+import numpy as np
+
+labels = [
+    "University of Alaska Anchorage",
+    "International Academy of Hair Design",
+    "University of Alabama in Huntsville",
+]
