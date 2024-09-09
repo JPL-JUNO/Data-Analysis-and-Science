6a505caa6bbfd247517486510ed37c0a43a1076c
Stephen CUI <junocuixuan@163.com>
2024-09-09 15:14:26 +0800

    xlwings模块与pandas模块的交互

--- PYXLF/sources/xlwings_pd.py
+++ PYXLF/sources/xlwings_pd.py
@@ -1,3 +1,12 @@
+"""
+@File         : xlwings_pd.py
+@Author(s)    : Stephen CUI
+@LastEditor(s): Stephen CUI
+@CreatedTime  : 2024-09-09 15:14:00
+@Email        : cuixuanstephen@gmail.com
+@Description  : xlwings 模块与 pandas 模块的交互
+"""
+
 import xlwings as xw
 import pandas as pd
 
