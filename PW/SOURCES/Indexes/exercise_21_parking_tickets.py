9744cc65906caa44a14ed380cd12ef2b844b653a
Stephen CUI <junocuixuan@163.com>
2024-10-05 22:58:06 +0800

    多层索引

--- PW/SOURCES/Indexes/exercise_21_parking_tickets.py
+++ PW/SOURCES/Indexes/exercise_21_parking_tickets.py
@@ -21,3 +21,31 @@ df = pd.read_csv(
         "Vehicle Color",
     ],
 )
+import numpy as np
+
+g = np.random.default_rng(0)
+df = pd.DataFrame(g.integers(0, 100, [36, 3]), columns=list("ABC"))
+df["year"] = [2018] * 12 + [2019] * 12 + [2020] * 12
+df["month"] = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split() * 3
+
+df = df.set_index("year")
+
+df.loc[
+    [
+        (2018, "Jun"),
+        (2018, "Jul"),
+        (2018, "Aug"),
+        (2019, "Jun"),
+        (2019, "Jul"),
+        (2019, "Aug"),
+        (2020, "Jun"),
+        (2020, "Jul"),
+        (2020, "Aug"),
+    ]
+]
+
+df.loc[([2018, 2019, 2020], ["Jun", "Jul", "Aug"]), ["A", "B", "C"]]
+
+df.loc[([2018, 2019, 2020], ["Jun", "Jul", "Aug"]), "A":"C"]
+
+df.loc[(slice(None), ["Jun", "Jul", "Aug"]), "A":"C"]
